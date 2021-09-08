1869c818f8c89a4bf34587ce4e9248c02e8529c8
Xueqin Li <54841759+lixueqin-4119@users.noreply.github.com>
2021-09-09 01:07:47 +0800

    Vue表单的基本用法

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <input v-model="message" placeholder="edit me" />
+   <br>
+   <textarea v-model="message" placeholder="复选框"></textarea>
+   <p>Message is: {{ message }}</p>
+   <p><button @click="message='李雪芹'">set message to lixueqin</button></p>
+  <hr>
+  复选框 (Checkbox)<br>
+  你去哪儿旅游?{{x}}
+  <br>
+  <label> 
+   <input type="checkbox" v-model="x" :value="1"/>
+   <span>北京环球影城</span>
+  </label>
+  <label> 
+   <input type="checkbox" v-model="x" :value="2"/>
+   <span>大海</span>
+  </label>
+  <label> 
+   <input type="checkbox" v-model="x"  :value="3"/>
+   <span>上海迪士尼</span>
+  </label>
+  <hr/>
+  单选框 (Radio)<br>
+  你想去哪儿?{{y}}
+  <br>
+  <label> 
+   <input type="radio" nane="want" v-model="y" :value="1"/>
+   <span>北京环球影城</span>
+  </label>
+  <label> 
+   <input type="radio" nane="want" v-model="y" :value="2"/>
+   <span>大海</span>
+  </label>
+  <label> 
+   <input type="radio" nane="want" v-model="y"  :value="3"/>
+   <span>上海迪士尼</span>
+  </label>
+  <hr/>
+  选择框 (Select)<br>
+  你想去哪儿?{{z}}<br>
+   <select  v-model="z">
+    <option :value="1">环球影城</option>
+    <option :value="2">大海</option>
+    <option :value="3">迪士尼</option>
+  </select>
+  <hr/>
+  多选multiple<br>
+  用 v-for 渲染的动态选项<br>
+  你想去哪儿?{{w}}<br>
+   <select multiple v-model="w">
+    <option v-for="item in array" :value="item.value" :key="item.value">
+    {{item.text}}
+    </option>
+  </select>
+  <hr/>
+  from<br>
+  登陆
+  <form @submit.prevent="onSubmit">
+   <label>
+     <span>用户名：</span>
+     <input type="text" v-model="user.username" />
+   </label>
+   <label>
+     <span>密码：</span>
+     <input type="password" v-model="user.password" />
+   </label>
+   <button type="submit">提交</button>
+  </form>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+     message:'hello',
+     array:[
+     {text:"环球影城",value:1},
+     {text:"大海",   value:2},
+     {text:"迪士尼",  value:3}
+     ],
+     x:[],
+     y:'',
+     z:'',
+     w:[],
+     user:{
+       username:'',
+       password:''
+     }
+    }
+  },
+  methods:{
+    onSubmit(){
+      console.log(this.user)
+    }
   }
-}
+  ,
+  components: {}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
